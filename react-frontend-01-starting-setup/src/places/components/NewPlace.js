3b0a397629ffebb4e4cfe22e37f578e7e7c0266a
venky.git <venkateshnarra368@gmail.com>
2025-05-20 10:56:21 -0500

    sharing input values and adding multiple inputs.

--- react-frontend-01-starting-setup/src/places/components/NewPlace.js
+++ react-frontend-01-starting-setup/src/places/components/NewPlace.js
@@ -1,6 +1,6 @@
 import React , {useCallback}from 'react';
 import './NewPlace.css';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators'; 
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'; 
 import Input from '../../shared/components/FormElements/Input_temp1';
 
 const NewPlace = () => {
@@ -8,11 +8,20 @@ const NewPlace = () => {
     const titleInputHandler = useCallback((Id, value, isValid) => {
 
     }, []);
+
+    const descriptionnputHandler = useCallback((Id, value, isValid) => {
+
+    }, []);
+
     return ( <form className='place-form'>
         
-        <Input element= "input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]}
+        <Input id= "title" element= "textarea" label="Title" validators={[VALIDATOR_REQUIRE()]}
          errorText="Please enter a valid Title."
          onInput = {titleInputHandler} />
+
+        <Input id="Description" element= "textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]}
+         errorText="Please enter a valid Description (at least 5 characters)."
+         onInput = {descriptionnputHandler} />
 
     </form>
     );
