58322355b3557d2c8e82c54ef3a84796cdfecbdf
venky.git <venkateshnarra368@gmail.com>
2025-05-21 13:50:03 -0500

    getting a place by user id. setting route for userid.

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -27,4 +27,16 @@ router.get('/:pid',(req, res, next) => {
 
 
 });
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+
+    });
+    res.json({place});
+
+
+});
 module.exports = router;
