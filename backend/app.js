da332ccff7b917424dfb5c7fd93ccb0554cc6802
venky.git <venkateshnarra368@gmail.com>
2025-05-21 13:42:08 -0500

    adding specific routes for places id --dummy for now.

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placesRoutes)
+app.use('/api/places', placesRoutes);
 
 
 
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    console.log('GET REQUEST in places');
-    res.json({message : "It works!"});
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title: ' Veritis building',
+        description: 'Work building',
+        location: {
+            lat: 32.8925184,
+            lng: -96.976896
+        },
+        address: '141 Greenway drive',
+        creator: 'u1'
+
+    }
+];
+
+router.get('/:pid',(req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId
+    });
+    res.json({place});
 
 
 
