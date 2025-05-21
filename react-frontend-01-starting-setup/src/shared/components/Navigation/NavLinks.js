326bb40b816aad52469c8d737c80da698688ce8d
venky.git <venkateshnarra368@gmail.com>
2025-05-21 09:53:54 -0500

    making sure only all users and Authentications nav links are visible first.

--- react-frontend-01-starting-setup/src/shared/components/Navigation/NavLinks.js
+++ react-frontend-01-starting-setup/src/shared/components/Navigation/NavLinks.js
@@ -1,21 +1,24 @@
 import React from "react";
 import {NavLink} from 'react-router-dom';
+import { AUthContext } from "../../context/auth-context";
 import './NavLinks.css';
+import { useContext } from 'react';
 
 const NavLinks = props => {
+     const auth = useContext(AUthContext);
     return <ul  className="nav-links">
         <li>
         <NavLink to = "/" exact> All Users</NavLink>
         </li>
-        <li>
+        {auth.isLoggedIn && (<li>
             <NavLink to = "/u1/places"> My Places</NavLink>
-        </li>
-        <li>
+        </li> )}
+        {auth.isLoggedIn && (<li>
             <NavLink to = "/places/new"> Add Places</NavLink>
-        </li>
-        <li>
+        </li>)}
+        {!auth.isLoggedIn && (<li>
             <NavLink to = "/auth"> Authenticate</NavLink>
-        </li>
+        </li>)}
 
     </ul>
 
