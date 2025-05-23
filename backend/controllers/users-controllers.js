0c89160119c750b1f530cee8af0da87636851e21
venky.git <venkateshnarra368@gmail.com>
2025-05-23 14:22:59 -0500

    getting all the users(used find and map),

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -19,8 +19,16 @@ const DUMMY_USERS = [
 ];
 
 
-const getUsers = (req, res, next) => {
-    res.status(200).json({users: DUMMY_USERS});
+const getUsers =async (req, res, next) => {
+    let users;
+    try{
+         users =  await User.find({} , '-password') ;
+    } catch (err) {
+        const error = new HttpError('fetching users failed!', 500);
+        return next(error);
+    }
+    res.json({users: users.map(user => user.toObject({getters: true}))});
+   
 };
 
 const signup = async (req, res, next) => {
