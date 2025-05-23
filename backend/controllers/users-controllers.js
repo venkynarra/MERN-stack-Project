644df1b0b442419715cfc75ccea1f8548d1600b3
venky.git <venkateshnarra368@gmail.com>
2025-05-23 14:12:25 -0500

    adding the user login .

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -32,6 +32,7 @@ const signup = async (req, res, next) => {
 
 
     const{ name, email, password, places} = req.body;
+
 let existingUser
     try{
          existingUser = await User.findOne({email: email})
@@ -64,13 +65,30 @@ let existingUser
  res.status(201).json({user: createdUser.toObject({getters: true})});
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
 const {email, password} = req.body;
 
-const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-if (!identifiedUser || identifiedUser.password !== password ){
-    throw new HttpError('Could not identify user! credentails are wrong.', 401);
-}
+let existingUser;
+
+    try{
+         existingUser = await User.findOne({email: email})
+    }catch(err){
+        const error = new HttpError('Login Failed! please try agaion later', 500);
+        return next(error);
+    }
+
+    if(existingUser){
+        const error = new HttpError('user exist already! please log in', 422);
+        return next(error);
+    }
+
+    if(!existingUser || existingUser.password !== password){
+        const error = new HttpError('Inavlid Credentials, could not log in' ,401);
+        return next(error);
+    }
+
+
+
 res.json({message: 'Logged in!'});
 
 };
