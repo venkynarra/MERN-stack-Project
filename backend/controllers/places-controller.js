33c93fe3818818e793e4e6e6deef7d1d91ada68f
venky.git <venkateshnarra368@gmail.com>
2025-05-23 12:17:34 -0500

    updating the user places.crud operations using postamn.

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -121,7 +121,7 @@ const createPlace = async(req, res, next) => {
 };
 
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         
@@ -130,15 +130,28 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatePlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId); // ✅ use findIndex
 
-  updatePlace.title = title;
-  updatePlace.description = description;
+  let place;
+  try{
+    place =  await Place.findById(placeId);
+  }catch (err){
+    const error = new HttpError('something went wrong could not update!', 500);
+    return next(error);
+  };
 
-  DUMMY_PLACES[placeIndex] = updatePlace;
+ 
 
-  res.status(200).json({ place: updatePlace });
+   place.title = title;
+   place.description = description;
+
+  try{
+    await place.save();
+  } catch (err){
+    const error = new HttpError('something went wrong cpuld not update the place!', 500);
+    return next(error);
+  }
+
+  res.status(200).json({ place:  place.toObject({getters: true}) });
 };
 
 
