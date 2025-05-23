531a0f61761dbd149f0d4e43556f9b3980fb093b
venky.git <venkateshnarra368@gmail.com>
2025-05-23 10:36:22 -0500

    created schema, created model and created documents in our database.

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -3,7 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const {validationResult} = require('express-validator')
 const HttpError = require('../models/http-error');
-const getCoordsForAddress = require('../util/location')
+const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place')
 
 
 let DUMMY_PLACES = [
@@ -77,17 +78,28 @@ const createPlace = async(req, res, next) => {
     }
     
     
-    const createdPlace = {
-        id: uuidv4(),
+    const createdPlace = new Place ({
         title,
         description,
-        location: coordinates,
         address,
+        location: coordinates,
+        image: 'https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqtfvwK0cjXUWR6D0HAAVBUkXZYj7t1es10q3UYUfhtk-r5hxPYtei_oSLfQUvyNeGi3S2VIe3u2XPrkmN_BRb82BcVeOACfUJhK4ig2QLCkau6u6GEjQmGvGdzCHdW9Jyzzok8=s680-w680-h510-rw',
         creator
+                
+    });
 
+     try{
 
-    };
-    DUMMY_PLACES.push(createdPlace);
+     
+     await createdPlace.save(); //saving a files to the database , async makes it fast 
+     } catch(err){
+        const error = new HttpError(
+            'creating place failed please try again', 500
+        );
+        return next(error);
+
+     }
+
     res.status(201).json({place: createdPlace});
 
 };
