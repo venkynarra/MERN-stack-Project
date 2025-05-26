43d06563cb653c98577ce1c5b675acac28d5fc71
venky.git <venkateshnarra368@gmail.com>
2025-05-26 10:52:30 -0500

    updating the places.

--- react-frontend-01-starting-setup/src/places/components/pages/UpdatePlaces.js
+++ react-frontend-01-starting-setup/src/places/components/pages/UpdatePlaces.js
@@ -1,43 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
 
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../../shared/components/FormElements/Input_temp1";
 import Button from "../../../shared/components/FormElements/Button";
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../../shared/util/validators";
 import { useForm } from "../../../shared/hooks/form-hook";
+import { useHttpClient } from "../../../shared/hooks/http-hooks";
+import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../../shared/components/UIElements/ErrorModal";
+import { AUthContext } from "../../../shared/context/auth-context";
 import './PlaceForm.css'
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'best sky scrapers',
-        imageUrl: '/images/venky.png',
-        address: '20 W 34th St, New York, NY 10001',
-        location :{
-            lat: 40.748440,
-            lng: -73.9882393,
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'Sky Scrapers in New York!',
-        imageUrl: '/images/venky.png',
-        address: '20 W 34th St, New York, NY 10001',
-        location :{
-            lat: 40.748440,
-            lng: -73.9882393,
-        },
-        creator: 'u2'
-    },
 
-];
+
 
 
 const UpdatePlace = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const auth = useContext(AUthContext);
+  const {isLoading, error, sendRequest, clearError} = useHttpClient();
+  const[loadedplaces , setLoadedPlaces] = useState();
+  
     const placeId = useParams().placeId;
+    const history = useHistory();
 
     
 
@@ -53,29 +36,52 @@ const UpdatePlace = () => {
             isValid: false
         }
     }, false);
-    const identifiedplace = DUMMY_PLACES.find(p =>p.id === placeId);
-useEffect(() => {
-    if (identifiedplace){
-        setFormData({
-        title:{
-            value: identifiedplace.title,
-            isValid: true
+   
+
+   useEffect(() => {
+  const fetchPlaces = async () => {
+    try {
+      const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`);
+      setLoadedPlaces(responseData.place);
+      setFormData({
+        title: {
+          value: responseData.place.title,
+          isValid: true
         },
-        description:{
-            value: identifiedplace.description ,
-            isValid: true
+        description: {
+          value: responseData.place.description,
+          isValid: true
         }
-    }, true);
-
+      }, true);
+    } catch(err) {
+      // handle error
     }
+  };
+
+  fetchPlaces(); // 
+}, [sendRequest, placeId, setFormData]);
+
+
+
     
 
-setIsLoading(false);
-}, [setFormData, identifiedplace]);
+
     
-    const placeUpdateSubmitHandler = event => {
+    const placeUpdateSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs)
+        try{
+          await sendRequest(`http://localhost:5000/api/places/${placeId}`, 'PATCH', JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value
+        }), {
+          'Content-Type': 'application/json'
+        });
+        history.push('/'+ auth.userId + '/places');
+        }catch (err){
+
+        }
+        
+        
 
     };
 
@@ -83,22 +89,25 @@ setIsLoading(false);
      if (isLoading) {
   return (
     <div className="center">
+      <LoadingSpinner/>
       <h2>Loading...</h2>
+
     </div>
   );
 }
 
-if (!identifiedplace) {
+if (!loadedplaces && !error) {
   return (
     <div className="center">
       <h2>Could not find the place!</h2>
     </div>
   );
 }
-    return (
+    return ( <React.Fragment>
+      <ErrorModal error = {error} onClear = {clearError} />
         
   <div className="center">
-    <form className="place-form" onSubmit = {placeUpdateSubmitHandler}>
+    {!isLoading && loadedplaces && (<form className="place-form" onSubmit = {placeUpdateSubmitHandler}>
       {/* Title */}
       <Input
         id="title"
@@ -108,8 +117,8 @@ if (!identifiedplace) {
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
         onInput={InputHandler}
-        initialValue={formState.inputs.title.value}
-        initialValid={formState.inputs.title.isValid}
+        initialValue={loadedplaces.title}
+        initialValid={true}
       />
 
       {/* Description */}
@@ -120,16 +129,17 @@ if (!identifiedplace) {
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (min 5 characters)."
         onInput={InputHandler}
-        initialValue={formState.inputs.description.value}
-        initialValid={formState.inputs.description.isValid}
+        initialValue={loadedplaces.description}
+        initialValid={true}
       />
 
       {/* Submit */}
       <Button type="submit" disabled={!formState.isValid}>
         UPDATE PLACE
       </Button>
-    </form>
+    </form>)};
   </div>
+  </React.Fragment>
 );
 
 
