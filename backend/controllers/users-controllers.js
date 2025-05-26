1c330569bf7972e4e737782654c71949ecd5b934
venky.git <venkateshnarra368@gmail.com>
2025-05-26 18:39:55 -0500

    using multer to save files and filtering file on the backend.

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -46,7 +46,7 @@ let existingUser
     const createdUser = new User({
         name, 
         email,
-        image:'https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F674010%2Fpexels-photo-674010.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-anjana-c-169994-674010.jpg%26fm%3Djpg&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeautiful%2F&docid=B51x0PBR9KNzvM&tbnid=sKMM4eBjWSQEBM&vet=12ahUKEwjdtqaqnbqNAxUHGtAFHTtxBxkQM3oECBwQAA..i&w=2976&h=3968&hcb=2&ved=2ahUKEwjdtqaqnbqNAxUHGtAFHTtxBxkQM3oECBwQAA',
+        image:'https://i.ibb.co/8sR6cYX/default-profile.png',
         password,
         places: []
     });
@@ -61,32 +61,32 @@ let existingUser
 };
 
 const login = async (req, res, next) => {
-const {email, password} = req.body;
+  const { email, password } = req.body;
 
-let existingUser;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError('Login failed! Please try again later.', 500);
+    return next(error);
+  }
 
-    try{
-         existingUser = await User.findOne({email: email})
-    }catch(err){
-        const error = new HttpError('Login Failed! please try agaion later', 500);
-        return next(error);
-    }
+  // ❌ BAD: You were wrongly handling the "user exists" case
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError('Invalid credentials, could not log you in.', 401);
+    return next(error);
+  }
 
-    if(existingUser){
-        const error = new HttpError('user exist already! please log in', 422);
-        return next(error);
-    }
-
-    if(!existingUser || existingUser.password !== password){
-        const error = new HttpError('Inavlid Credentials, could not log in' ,401);
-        return next(error);
-    }
-
-
-
-res.json({message: 'Logged in!'});
-
+  // ✅ Login Success
+  res.json({ message: 'Logged in!', user: existingUser.toObject({getters: true}) });
 };
+
+
+
+
+
+
+
 
 exports.getUsers = getUsers;
 exports.signup = signup;
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 const userController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -10,7 +11,10 @@ router.get('/', userController.getUsers);
 
 
 
-router.post('/signup',[
+router.post('/signup',
+    fileUpload.single('image'),
+    
+    [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
     check('password').isLength({min: 6})
