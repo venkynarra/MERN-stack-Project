74284b4e62594ecb48d140f755d116603345cc5f
venky.git <venkateshnarra368@gmail.com>
2025-05-27 10:47:20 -0500

    generating jwt tokens in the users- controllers for signup and login.

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -5,6 +5,7 @@ const HttpError = require('../models/http-error');
 const { validationResult } = require('express-validator');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const getUsers =async (req, res, next) => {
     let users;
@@ -70,8 +71,20 @@ let existingUser
     const error = new HttpError('signing up failed, please try again!', 500);
     return next(error);
   }
+  let token;
+  try{
+    token = jwt.sign({userId: createdUser.id , email: createdUser.email}, 'supersecret_dont_share', {expiresIn: '1h'});
 
- res.status(201).json({user: createdUser.toObject({getters: true})});
+
+  }catch(err){
+    const error = new HttpError('signing up failed, please try again!', 500);
+    return next(error);
+
+  }
+  
+
+
+ res.status(201).json({userId: createdUser.id, email: createdUser.email, token: token});
 };
 
 const login = async (req, res, next) => {
@@ -85,14 +98,40 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  // ❌ BAD: You were wrongly handling the "user exists" case
-  if (!existingUser || existingUser.password !== password) {
-    const error = new HttpError('Invalid credentials, could not log you in.', 401);
-    return next(error);
-  }
 
+  
+  if (!existingUser) {
+     const error = new HttpError('Invalid credentials, could not log you in.', 401);
+    return next(error);
+   
+    
+  }
+  let isValidPassword = false;
+  try{
+
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+  }catch(err) {
+     const error = new HttpError('Login failed! please check your credentials and try again', 500);
+    return next(error);
+
+  }
+if(!isValidPassword){
+   const error = new HttpError('Invalid credentials, could not log you in.', 401);
+    return next(error);
+
+}
+let token;
+  try{
+    token = jwt.sign({userId: existingUser.id , email: existingUser.email}, 'supersecret_dont_share', {expiresIn: '1h'});
+
+
+  }catch(err){
+    const error = new HttpError('signing up failed, please try again!', 500);
+    return next(error);
+
+  }
   // ✅ Login Success
-  res.json({ message: 'Logged in!', user: existingUser.toObject({getters: true}) });
+  res.json({ userId: existingUser.id, email: existingUser.email, token: token });
 };
 
 
